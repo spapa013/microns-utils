103c77fd3bc4ff46c5e583262eaed5ccf118cac2
spapa013 <stelios@spapa.us>
2021-11-09 10:09:55 -0600

    adds _get_calling_context

--- microns_utils/config_utils.py
+++ microns_utils/config_utils.py
@@ -28,6 +28,17 @@ def register_externals(external_stores):
         dj.config['stores'].update(external_stores)
 
 
+def _get_calling_context() -> locals:
+    # get the calling namespace
+    import inspect
+    try:
+        frame = inspect.currentframe().f_back
+        context = frame.f_locals
+    finally:
+        del frame
+    return context
+
+
 def register_adapters(adapter_objects, context=None):
     """
     Imports the adapters for a schema_name into the global namespace.
@@ -35,9 +46,11 @@ def register_adapters(adapter_objects, context=None):
     if context is None:
         # if context is missing, use the calling namespace
         import inspect
-        frame = inspect.currentframe().f_back
-        context = frame.f_locals
-        del frame
+        try:
+            frame = inspect.currentframe().f_back
+            context = frame.f_locals
+        finally:
+            del frame
     
     for name, adapter in adapter_objects.items():
         context[name] = adapter
@@ -45,10 +58,12 @@ def register_adapters(adapter_objects, context=None):
 
 def create_vm(schema_name:str, external_stores=None, adapter_objects=None):
     """
-    Creates a virtual module after registering the external stores, and includes the adapter objects in the vm.
+    Creates a virtual module after registering the external stores, and includes the adapter objects in the vm. 
+
+    Creating tables disabled from virtual modules. 
     """
     
     if external_stores is not None:
         register_externals(external_stores)
     
-    return dj.create_virtual_module(schema_name, schema_name, add_objects=adapter_objects)
+    return dj.create_virtual_module(schema_name, schema_name, add_objects=adapter_objects, create_tables=False)
