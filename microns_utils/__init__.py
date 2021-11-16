dff4988431cd2c7d6d51192d131fbb626c98d033
spapa013 <stelios@spapa.us>
2021-11-16 09:10:59 -0600

    import local version instead of checking metadata

--- microns_utils/__init__.py
+++ microns_utils/__init__.py
@@ -1,6 +1,5 @@
 from . import version_utils
-
-__version__ = version_utils.get_package_version(package='microns-utils')
+from .version import __version__
 
 def get_latest_version_from_github():
     return version_utils.get_latest_version_from_github(repo='microns-utils', user='cajal', branch='main', source='commit', path_to_version_file='microns_utils/version.py', tag=None, warn=True)
