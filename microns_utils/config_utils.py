02ca43ebce5784e7a691835bf3227f2ae1491972
spapa013 <stelios@spapa.us>
2021-12-20 17:26:46 -0600

    replaces config_utils with SchemaConfig class

--- microns_utils/config_utils.py
+++ microns_utils/config_utils.py
@@ -1,131 +1,46 @@
 """
-Methods for configuring microns packages.
+Classes and methods for configuring microns packages.
 """
-
-import traceback
-import inspect
-try:
-    import datajoint as dj
-except:
-    traceback.print_exc()
-    raise ImportError('DataJoint package not found.')
 import datajoint.datajoint_plus as djp
 
-def enable_datajoint_flags(enable_python_native_blobs=True):
+class SchemaConfig:
     """
-    Enable experimental datajoint features
+    DataJoint schema configuration object.
+    """
+    def __init__(self, module_name:str, schema_name:str, externals:dict=None, adapters:dict=None):
+        """
+        Initializes DataJoint configuration object.
+        
+        :param module_name: name of Python module containing DataJoint tables
+        :param schema_name: name of DataJoint schema object
+        :param externals: dictionary of externals folders to map to schema
+        :param adapters: adapters to add to schema context
+        """
+        self._module_name = module_name
+        self._schema_name = schema_name
+        self._externals = externals
+        self._adapters = adapters
     
-    These flags are required by 0.12.0+ (for now).
-    """
-    dj.config['enable_python_native_blobs'] = enable_python_native_blobs
-    dj.errors._switch_filepath_types(True)
-    dj.errors._switch_adapted_types(True)
-
-
-def register_externals(external_stores):
-    """
-    Registers the external stores for a schema_name in this module.
-    """
-    if 'stores' not in dj.config:
-        dj.config['stores'] = external_stores
-    else:
-        dj.config['stores'].update(external_stores)
-
-
-def make_store_dict(path):
-    return {
-        'protocol': 'file',
-        'location': str(path),
-        'stage': str(path)
-    }
-
-
-def _get_calling_context() -> locals:
-    # get the calling namespace
-    import inspect
-    try:
-        frame = inspect.currentframe().f_back
-        context = frame.f_locals
-    finally:
-        del frame
-    return context
-
-
-def register_adapters(adapter_objects, context=None):
-    """
-    Imports the adapters for a schema_name into the global namespace.
-    """   
-    if context is None:
-        # if context is missing, use the calling namespace
-        import inspect
-        try:
-            frame = inspect.currentframe().f_back
-            context = frame.f_locals
-        finally:
-            del frame
+    @property
+    def module_name(self):
+        return self._module_name
     
-    for name, adapter in adapter_objects.items():
-        context[name] = adapter
-
-
-djp_mapping = {
-    'Lookup': djp.Lookup,
-    'Manual': djp.Manual,
-    'Computed': djp.Computed,
-    'Imported': djp.Imported,
-    'Part': djp.Part
-}
-
-
-djp_virtual_mapping = {
-    'Lookup': djp.VirtualLookup,
-    'Manual': djp.VirtualManual,
-    'Computed': djp.VirtualComputed,
-    'Imported': djp.VirtualImported,
-    'Part': djp.VirtualPart
-}
-
-
-def add_datajoint_plus(module, virtual=False):
-    """
-    Adds DataJointPlus recursively to the DataJoint tables inside the module.
-    """
-    for name in dir(module):
-        obj = getattr(module, name)
-        if virtual:
-            if inspect.isclass(obj) and issubclass(obj, dj.Table) and not issubclass(obj, djp.VirtualModule):
-                obj.__bases__ = (djp_virtual_mapping[obj.__base__.__name__],)
-                obj.parse_hash_info_from_header()
-                add_datajoint_plus(obj)
-        else:
-            if inspect.isclass(obj) and issubclass(obj, dj.Table) and not issubclass(obj, djp.Base):
-                obj.__bases__ = (djp_mapping[obj.__base__.__name__],) if not virtual else (djp_virtual_mapping[obj.__base__.__name__],)
-                obj.parse_hash_info_from_header()
-                add_datajoint_plus(obj)
-
-
-def reassign_master_attribute(module):
-    """
-    Overwrite .master attribute in DataJoint part tables to map to master class from current module instead of inherited module
-    """
-    for name in dir(module):
-        # Get DataJoint tables
-        if inspect.isclass(getattr(module, name)) and issubclass(getattr(module, name), dj.Table):
-            obj = getattr(module, name)
-            for nested in dir(obj):
-                # Get Part tables
-                if inspect.isclass(getattr(obj, nested)) and issubclass(getattr(obj, nested), dj.Part):
-                    setattr(getattr(obj, nested), '_master', obj)
-
-
-def _create_vm(schema_name:str, external_stores=None, adapter_objects=None):
-    """
-    Creates a virtual module after registering the external stores, and includes the adapter objects in the vm. 
-
-    Creating tables disabled from virtual modules. 
-    """
+    @property
+    def schema_name(self):
+        return self._schema_name
     
-    if external_stores is not None:
-        register_externals(external_stores)
+    @property
+    def externals(self):
+        return self._externals
     
-    return dj.create_virtual_module(schema_name, schema_name, add_objects=adapter_objects, create_tables=False)
+    @property
+    def adapters(self):
+        return self._adapters
+    
+    def register_externals(self):
+        if self.externals is not None:
+            djp.register_externals(self.externals)
+    
+    def register_adapters(self, context=None):
+        if self.adapters is not None:
+            djp.add_objects(self.adapters, context=context)
