418e3547c3a88ea1cd051fb4e749bb8f0dc1fdfb
spapa013 <stelios@spapa.us>
2021-12-22 16:49:01 -0600

    update check_latest_version_from_github to use wrapper

--- microns_utils/__init__.py
+++ microns_utils/__init__.py
@@ -1,8 +1,4 @@
 from . import version_utils
 from .version import __version__
 
-def check_latest_version_from_github(owner='cajal', repo='microns-utils', source='tag', branch=None, path_to_version_file=None, warn=True):
-    """
-    Wrapper for :func:`~version_utils.check_latest_version_from_github`
-    """
-    return version_utils.check_latest_version_from_github(owner=owner, repo=repo, source=source, branch=branch, path_to_version_file=path_to_version_file, warn=warn)
+check_latest_version_from_github = version_utils.latest_github_version_checker(owner='cajal', repo='microns-utils')
