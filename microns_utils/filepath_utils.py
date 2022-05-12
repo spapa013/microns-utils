28844403a97b4220d3325e371a6ac80b815a2097
spapa013 <stelios@spapa.us>
2022-05-12 06:04:01 -0500

    adds timezone converter, modifies append_timestamp_to_filepath

--- microns_utils/filepath_utils.py
+++ microns_utils/filepath_utils.py
@@ -28,6 +28,26 @@ def validate_filepath(filepath):
     return filepath
 
 
+def timezone_converter(timestamp, source_tz, destination_tz, fmt=None):
+    """
+    Converts timestamp from a source timezone to a destination timezone. 
+    To see commonly used timezone options run: 
+    
+    ```from pytz import common_timezones```
+    
+    :param timestamp: (datetime.datetime) timestamp to convert
+    :param source_tz: (pytz.timezone) source timezone to convert 
+    :param destination_tz: (pytz.timezone) destination timezone
+    :param fmt: (str) optional - timestamp format to pass to strftime
+    
+    :returns: (datetime.datetime) converted timestamp
+    """
+    dest_obj = pytz.timezone(destination_tz)
+    source_obj = pytz.timezone(source_tz).localize(timestamp.replace(tzinfo=None))
+    converted = dest_obj.normalize(source_obj.astimezone(dest_obj))
+    return converted if fmt is None else converted.strftime(fmt)
+
+
 def get_file_modification_time(filepath, timezone, fmt=None):
     """
     Gets modification time of file.
@@ -39,23 +59,22 @@ def get_file_modification_time(filepath, timezone, fmt=None):
     :returns: datetime object
     """
     ts = datetime.datetime.fromtimestamp(Path(filepath).stat().st_mtime, tz=datetime.timezone.utc)
-    localtz = pytz.timezone(timezone)
-    utctime = pytz.timezone('UTC').localize(ts.replace(tzinfo=None))
-    timestamp = localtz.normalize(utctime.astimezone(localtz))
-    return timestamp if fmt is None else timestamp.strftime(fmt)
+    return timezone_converter(ts, 'UTC', timezone, fmt=fmt)
 
 
-def append_timestamp_to_filepath(filepath, timestamp, separator='__', verbose=False, return_filepath=False):
+def append_timestamp_to_filepath(filepath, timestamp, separator='__', with_suffix=None, verbose=True, return_filepath=False):
     """
-    Appends timestamp to provided filepath.
+    Appends timestamp to provided filepath (but before the file extension)
     
     :param filepath: (patlib.Path or str) filepath to directory or file to modify
     :param timestamp: (str or datetime object) timestamp to append
+    :param with_suffix: (str) desired alternate file extension to replace existing extension. 
+        By default, original extension will be maintained.
     :param verbose: (bool) logs renamed filepath
     :param return_filepath: (bool) returns renamed filepath patlib.Path
     """
     filepath = Path(filepath)  
-    filepath_rn = filepath.with_name(f'{filepath.stem}{separator}{timestamp}')
+    filepath_rn = filepath.with_name(f'{filepath.stem}{separator}{timestamp}').with_suffix(filepath.suffix if with_suffix is None else with_suffix)
     filepath.rename(filepath_rn)
     if verbose:
         logging.info(f'File renamed: {filepath_rn}')
