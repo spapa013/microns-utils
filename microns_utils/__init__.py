295fd84b4e13eeb28653e52fb749198d45871acd
spapa013 <stelios@spapa.us>
2022-05-12 06:02:49 -0500

    remove configure_logger

--- microns_utils/__init__.py
+++ microns_utils/__init__.py
@@ -1,7 +1,4 @@
 from . import version_utils
 from .version import __version__
-from .logging_utils import configure_logger
-
-configure_logger()
 
 check_latest_version_from_github = version_utils.latest_github_version_checker(owner='cajal', repo='microns-utils')
