3c022f14f0ca6e3d41972e02cb6ec72b2d0c00a6
spapa013 <stelios@spapa.us>
2022-07-25 21:24:33 -0500

    update DashboardUser

--- microns_utils/dashboard_utils.py
+++ microns_utils/dashboard_utils.py
@@ -1,61 +1,64 @@
 from traitlets import Unicode, Dict, Unicode
 from ipywidgets import DOMWidget, register
+from IPython.core.magics.display import Javascript
 
 # CODE SNIPPET FOR FETCHING USERNAME
 # put in first cell of jupyter notebook
 # https://gist.github.com/danlester/ac1d5f29358ce1950482f8e7d4301f86
-"""
-%%javascript
+def get_user():
+    Javascript("""
+    %%javascript
 
-require.undef('user_widget');
+    require.undef('user_widget');
 
-define('user_widget', ["@jupyter-widgets/base"], function (widgets) {
+    define('user_widget', ["@jupyter-widgets/base"], function (widgets) {
 
-    
-    var UserView = widgets.DOMWidgetView.extend({
-        initialize: function(attributes, options) {
+        
+        var UserView = widgets.DOMWidgetView.extend({
+            initialize: function(attributes, options) {
 
-           this.response = fetch(
-                '/hub/dashboards-api/hub-info/user',
-                 { 
-                 mode: 'no-cors', 
-                 credentials: 'same-origin',
-                 headers: new Headers({'Access-Control-Allow-Origin':'*'}) 
-            });
-            this.response.then( response => {
-                this.result = response.json();
-                this.result.then( json => {
-                    this.model.set('value', json);
-                    this.model.set('name', json.name);
-                    this.model.save_changes();
+            this.response = fetch(
+                    '/hub/dashboards-api/hub-info/user',
+                    { 
+                    mode: 'no-cors', 
+                    credentials: 'same-origin',
+                    headers: new Headers({'Access-Control-Allow-Origin':'*'}) 
                 });
+                this.response.then( response => {
+                    this.result = response.json();
+                    this.result.then( json => {
+                        this.model.set('value', json);
+                        this.model.set('name', json.name);
+                        this.model.save_changes();
+                    });
 
-            });
-        
-        },
-        
-        render: async function () {
-            await this.response;
-            await this.result;
-            var json = this.model.get('value');
-            var text = 'No user';
-            if (json.hasOwnProperty('name')) {
-                text = 'Rendered by Javascript: '+json['name'];
-            }
-            this.el.appendChild(document.createTextNode(text));
+                });
+            
+            },
+            
+            render: async function () {
+                await this.response;
+                await this.result;
+                var json = this.model.get('value');
+                var text = 'No user';
+                if (json.hasOwnProperty('name')) {
+                    text = 'Rendered by Javascript: '+json['name'];
+                }
+                this.el.appendChild(document.createTextNode(text));
 
-        },
-       
+            },
+        
+        });
+
+        return {
+            UserView: UserView
+        };
     });
-
-    return {
-        UserView: UserView
-    };
-});
-"""
+    """)
 
 @register
 class DashboardUser(DOMWidget):
+    get_user()
     # https://gist.github.com/danlester/ac1d5f29358ce1950482f8e7d4301f86
     _view_name = Unicode('UserView').tag(sync=True)
     _view_module = Unicode('user_widget').tag(sync=True)
