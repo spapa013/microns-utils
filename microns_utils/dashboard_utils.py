52d117ba7349646cf8dca87f33f8b6fc544330c4
spapa013 <stelios@spapa.us>
2022-07-29 15:39:18 -0500

    updates dashboard_utils.py

--- microns_utils/dashboard_utils.py
+++ microns_utils/dashboard_utils.py
@@ -1,6 +1,7 @@
 from traitlets import Unicode, Dict, Unicode
 from ipywidgets import DOMWidget, register, link
 import wridgets.app as wra
+import json
 
 jupyerhub_get_user_info = """
     require.undef('user_widget');
@@ -67,7 +68,7 @@ class DashboardUser(DOMWidget):
 class UserApp(wra.App):
     store_config = [
         'user',
-        'user_info'
+        'user_info',
         'user_app'
     ]
     
@@ -76,19 +77,21 @@ class UserApp(wra.App):
         
         self.core = (
             wra.Label(text='User', name='UserLabel') + \
-            wra.Field(disabled=True, name='UserField')
+            wra.Field(disabled=True, name='UserField', on_interact=self.on_user_field_update) + \
+            wra.Field(disabled=True, name='UserInfoField', layout={'display': 'none'}, value='{}', wridget_type='Textarea', on_interact=self.on_user_info_field_update)
         )
         
         if 'user_app' in kwargs:
             self.user_app = kwargs.get('user_app')
             link((self.children.UserField.wridget.widget, 'value'), (self.user_app, 'name'))
+            link((self.children.UserInfoField.wridget.widget, 'value'), (self.user_app, 'value'), transform=[json.loads, json.dumps])
             
         elif 'user_info' in kwargs:
-            self.user_info = kwargs.get('user_info')
-            self.user = self.user_info.get('name')
-            self.children.UserField.set(value=self.user)
-            
-    def set_user_from_user_app(self):
-        assert self.user_app is not None, 'Cant set user_info because user_app is None'
-        self.user = self.user_app.value.get('name')
-        self.user_info = self.user_app.value
+            self.children.UserField.set(value=kwargs.get('user_info').get('name'))
+            self.children.UserInfoField.set(value=json.dumps(kwargs.get('user_info')))
+
+    def on_user_field_update(self):
+        self.user = self.children.UserField.get1('value')
+    
+    def on_user_info_field_update(self):
+        self.user_info = json.loads(self.children.UserInfoField.get1('value'))
