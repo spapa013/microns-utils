cab3e97370ab55560e726c335476d9d942d7ce77
spapa013 <stelios@spapa.us>
2023-05-03 10:41:58 -0500

    remove djp import dependency

--- microns_utils/misc_utils.py
+++ microns_utils/misc_utils.py
@@ -1,4 +1,29 @@
-from datajoint_plus.utils import classproperty, wrap, unwrap
+"""
+Misc utils
+"""
+
+class classproperty:
+    def __init__(self, f):
+        self.f = f
+
+    def __get__(self, obj, owner):
+        return self.f(owner)
+
+
+def wrap(item):
+    if isinstance(item, (list, tuple)):
+        return item
+    else:
+        return item,
+
+
+def unwrap(item):
+    if isinstance(item, (list, tuple)):
+        if len(item) == 1:
+            return item[0]
+    else:
+        return item
+    
 
 def sc_to_ucc(string):
     """
